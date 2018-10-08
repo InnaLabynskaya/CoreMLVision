537f3210b44af33cee0f506a6041a22c3e215f5a
Inna Kuts <i.kuts@nullgr.com>
2018-10-08 13:54:52 +0300

    Initial Commit

---
+++ CoreMLVision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLVision
+//
+//  Created by Inna Kuts on 10/8/18.
+//  Copyright © 2018 Inna Kuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
